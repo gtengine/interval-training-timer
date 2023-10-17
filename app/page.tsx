3d1de1bd4189052b3b3bed4fa060c8dfd18ddd16
gtengine <prierkt@gmail.com>
2023-10-17 09:40:52 +0000

    a

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
 import Timer from "@/components/Timer";
 import TimeList from "@/components/TimeList";
+import Margin from "@/components/Margin";
 
 export default function Home() {
   return (
     <main className="lg:flex justify-between">
       <Timer />
+
+      {/* mobile margin */}
+      <Margin className="h-8" />
+
       <TimeList />
     </main>
   );
